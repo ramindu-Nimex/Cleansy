1d484f5e8737787b6cd3b5e4e5a7c66d7bb578a8
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2025-09-21 11:30:18 +0530

    feat:OAuth Connection established

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,18 +1,19 @@
-// Import the necessary Firebase modules
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Your Firebase app's configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "cleansy-ea4f4.firebaseapp.com",
-  projectId: "cleansy-ea4f4",
-  storageBucket: "cleansy-ea4f4.appspot.com",
-  messagingSenderId: "407073933828",
-  appId: "1:407073933828:web:a05057979e13ff59c8c351",
+  authDomain: "cleansy-ssd.firebaseapp.com",
+  projectId: "cleansy-ssd",
+  storageBucket: "cleansy-ssd.firebasestorage.app",
+  messagingSenderId: "406078280471",
+  appId: "1:406078280471:web:bcbd0a7d71b47f66b39d52",
+  measurementId: "G-5S7NBP3KPQ"
 };
 
-// Initialize Firebase app
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-

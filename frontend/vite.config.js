5090a1bb9cbf66c344ea1cbddf6b6baa87f27bec
samidisjg <samidisjg@gmail.com>
2025-09-24 15:56:31 +0530

    feat(dev): enable HTTPS in Vite using mkcert certificates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'  //  import fs
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      }
-    }
+  '/api': {
+    target: 'https://localhost:3000',
+    secure: false, // because it's self-signed
+  }
+}
+
   },
   plugins: [react()],
 })
